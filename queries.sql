6b005ba49232f90d47b57ce51c14f301832ecb23
Cristian Villa <129917979+CVILLA09@users.noreply.github.com>
2023-09-26 22:39:25 -0600

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -37,3 +37,11 @@ UPDATE animals SET weight_kg = weight_kg * -1;
 ROLLBACK TO sp1;
 UPDATE animals SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
 COMMIT;
+
+-- New queries for final task
+SELECT COUNT(*) FROM animals;
+SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
+SELECT AVG(weight_kg) FROM animals;
+SELECT neutered, COUNT(*) FROM animals WHERE escape_attempts > 0 GROUP BY neutered;
+SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
