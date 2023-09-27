d4d0a36dca2be23bb78e31c0927473ca05a5a0a5
Cristian Villa <129917979+CVILLA09@users.noreply.github.com>
2023-09-27 12:37:40 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,13 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-  id INT, name CHAR(40), 
+  id INT, 
+  name CHAR(40), 
   date_of_birth DATE, 
   escape_attempts INT, 
   neutered BOOLEAN, 
-  weight_kg DECIMAL,
-  species CHAR(40)
+  weight_kg DECIMAL
 );
+
+-- Add new column species
+ALTER TABLE animals ADD COLUMN species CHAR(40);
