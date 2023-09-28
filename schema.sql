577db0ddcfc58d7763badf58e2bea4f77d9bae5d
Cristian Villa <129917979+CVILLA09@users.noreply.github.com>
2023-09-28 15:31:00 -0600

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE species (
 ALTER TABLE animals ADD COLUMN species_id INT REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INT REFERENCES owners(id);
 
--- Create a table named vets
+-- Create the vets table
 CREATE TABLE vets (
   id SERIAL PRIMARY KEY,
   name VARCHAR(100),
@@ -36,4 +36,17 @@ CREATE TABLE vets (
   date_of_graduation DATE
 );
 
+-- Create the specializations table
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets(id),
+  species_id INTEGER REFERENCES species(id),
+  PRIMARY KEY (vet_id, species_id)
+);
 
+-- Create the visits table
+CREATE TABLE visits (
+  animal_id INTEGER REFERENCES animals(id),
+  vet_id INTEGER REFERENCES vets(id),
+  visit_date DATE,
+  PRIMARY KEY (animal_id, vet_id, visit_date)
+);
